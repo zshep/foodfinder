e170814ad3f9e9ae90f0147b77060e3e487f6a6c
zsheperdigian <zsheperdigian@gmail.com>
2023-02-02 20:54:18 -0800

    in the middle of adding a post route for a food item. not yet finished

--- app/static/javascript/add-btn.js
+++ app/static/javascript/add-btn.js
@@ -1,11 +1,21 @@
 // grabbing variable for submit btn
 const submitbtn = document.querySelector('#submitbtn');
-
+const foodItem = document.querySelector('#newFood').ariaValueMax;
 
 
 function submitfood() {
     console.log('the submit button was hit')
+    console.log('the new food is...', foodItem.value)
 
+    fetch('/addfood', {
+
+        method: 'POST',
+        body: JSON.stringify({
+            foodItem
+        })
+
+
+    })
 
     return
 }
@@ -13,4 +23,4 @@ function submitfood() {
 
 
 
-submitbtn.addEventListener('click', submitfood)
+/* submitbtn.addEventListener('click', submitfood) */
